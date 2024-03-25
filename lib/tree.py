673c8686da7de297397007f4f00eff02be3edd96
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-03-25 18:02:15 +0300

    Completed Tree Traversal- Compulsory

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      
+      self.root = nodes_to_visit.pop(0)
+    
+      if self.root['id'] == id:
+        result.append(self.root)
+
+      nodes_to_visit = self.root['children'] + nodes_to_visit
+
+    return result[0] if result else None
